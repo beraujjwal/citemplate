c8c897081456ccbdd9565f460e0beca1a063404c
Sunil K Samanta <sunilthedj@gmail.com>
2018-01-30 18:53:45 +0530

    Final

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	function __construct(){
+		parent::__construct();
+		$this->load->section('header','welcome/header');
+		$this->load->section('sidebar','welcome/sidebar');
+		$this->load->section('footer','welcome/footer');
+		$this->output->set_title('Theism Admin');
+		$this->output->set_template('admin');
+	}
+
+
+
+	public function index()
+	{
+		$this->output->append_title('Welcome');
+		$this->load->view('welcome/sample_view');
+	}
+
+	public function table(){
+		$this->output->append_title('Table');
+		$this->load->css('assets/themes/admin/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css');
+		$this->load->js('assets/themes/admin/bower_components/datatables.net/js/jquery.dataTables.min.js');
+		$this->load->js('assets/themes/admin/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js');
+		$this->load->view('welcome/sample_table');
+
+	}
+
+
+
+
+}
---
+++ application/views/themes/admin.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php echo $title; ?></title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  <?php
+    if(!empty($meta))
+    foreach($meta as $name=>$content){
+      echo "\n\t\t";
+      ?><meta name="<?php echo $name; ?>" content="<?php echo $content; ?>" /><?php
+         }
+    echo "\n";
+
+    if(!empty($canonical))
+    {
+      echo "\n\t\t";
+      ?><link rel="canonical" href="<?php echo $canonical?>" /><?php
+
+    }
+    echo "\n\t";
+  ?>
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/themes/admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/themes/admin/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/themes/admin/bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/themes/admin/dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/themes/admin/dist/css/skins/_all-skins.min.css">
+
+  <?php 
+  foreach($css as $file){
+    echo "\n\t\t";
+    ?><link rel="stylesheet" href="<?php echo $file; ?>" type="text/css" /><?php
+  } echo "\n\t";
+
+  ?>
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+
+  <!-- jQuery 3 -->
+  <script src="<?php echo base_url();?>assets/themes/admin/bower_components/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="<?php echo base_url();?>assets/themes/admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- SlimScroll -->
+  <script src="<?php echo base_url();?>assets/themes/admin/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+  <!-- FastClick -->
+  <script src="<?php echo base_url();?>assets/themes/admin/bower_components/fastclick/lib/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="<?php echo base_url();?>assets/themes/admin/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="<?php echo base_url();?>assets/themes/admin/dist/js/demo.js"></script>
+
+  <?php 
+  foreach($js as $file){
+      echo "\n\t\t";
+      ?><script src="<?php echo $file; ?>"></script><?php
+  } echo "\n\t";
+
+  ?>
+
+
+
+  <script>
+    $(document).ready(function () {
+      $('.sidebar-menu').tree()
+    })
+  </script>
+
+
+
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <?php echo $this->load->get_section('header');?>
+  
+
+  <!-- =============================================== -->
+
+  <!-- Left side column. contains the sidebar -->
+  <?php echo $this->load->get_section('sidebar');?>
+  
+
+  <!-- =============================================== -->
+
+  <!-- Content Wrapper. Contains page content -->
+    <?php echo $output;?>
+  <!-- /.content-wrapper -->
+   <?php echo $this->load->get_section('footer');?>
+  
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+
+</div>
+<!-- ./wrapper -->
+
+
+</body>
+</html>
